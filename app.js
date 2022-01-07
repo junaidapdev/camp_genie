77748e7b71424777143a07ca4f1946ee169032cc
junaidapdev <junaidap.dev.com>
2022-01-07 22:55:56 +0530

    bootstrap added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require("path");
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 mongoose.connect('mongodb://localhost:27017/campgenie');
 
@@ -14,7 +15,7 @@ db.once("open", () => {
 
 const app = express();
 
-
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
