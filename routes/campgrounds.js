95249f5a3a6c31af0fab9e80b41dd593b7fb4660
junaidapdev <junaidap.dev.com>
2022-02-03 23:50:42 +0530

    review permissions added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,22 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const { campgroundSchema } = require('../schemas.js');
-const { isLoggedIn, isAuthor } = require('../middleware');
+const { isLoggedIn, isAuthor, validateCampground} = require('../middleware');
 
 
-const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
-
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
 
 router.get('/', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -44,7 +32,7 @@ router.get('/:id', catchAsync(async (req, res,) => {
             path: 'author'
         }
     }).populate('author');
-    console.log(campground);
+        console.log(campground);
         if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');
@@ -55,17 +43,20 @@ router.get('/:id', catchAsync(async (req, res,) => {
 
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
+    const { id } = req.params;
+    const campground = await Campground.findById(id)
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');
     }
+    
     res.render('campgrounds/edit', { campground });
 }))
 
 router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    
+    const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 }));
